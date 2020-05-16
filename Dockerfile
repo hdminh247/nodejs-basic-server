8c32ddff6256710d15324a4d9a6905b418f23909
MinHo <minh.hoang@beesightsoft.com>
2020-05-16 22:58:36 +0700

    Add socket

--- Dockerfile
+++ Dockerfile
@@ -11,7 +11,7 @@ COPY package*.json ./
 RUN npm install
 
 # Copy source codes
-COPY ./* .
+COPY ./server.js .
 
 # Exceute command
 
--- server.js
+++ server.js
@@ -3,11 +3,19 @@ let port = 2020;
 
 
 // Create http server
- http.createServer((req, res) => {
+const server = http.createServer((req, res) => {
      res.writeHead(200, { 'Content-Type': 'text/plain' });
      res.write('Hello');
      res.end()
 }).listen(port, ()=>{
     console.log('  Server is running at http://localhost:%d', port);
     console.log('  Press CTRL-C to stop\n');
+});
+
+// Init socket instance
+const io = require('socket.io')(server);
+
+// Listen on connection
+io.on('connection', client => {
+    console.log(`Client connected: SocketID = ${client.id}`);
 });
